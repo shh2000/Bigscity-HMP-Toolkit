aa8adc8fced3f9b7b77d7274cd687518a578338c
飞翔的小兔子 <1290830173@qq.com>
2020-08-23 12:37:59 +0800

    修改评估部分结构及文档说明

--- tasks/run_loc_pred_evaluate.py
+++ tasks/run_loc_pred_evaluate.py
@@ -1,17 +1,16 @@
-from evaluate.loc_pred_evaluate import loc_pred_evaluate_model as lpem
-import json
+from evaluate import loc_pred_evaluate_model as lpem
 
 if __name__ == '__main__':
     data = '{' \
            '"uid1": { ' \
            '"trace_id1":' \
-           '{ "loc_true": [1], "loc_pred": [[0.01, 0.1]] }, ' \
+           '{ "loc_true": [1], "loc_pred": [[0.01, 0.91, 0.8]] }, ' \
            '"trace_id2":' \
-           '{ "loc_true": [2], "loc_pred": [[0.2, 0.13]] } ' \
+           '{ "loc_true": [2], "loc_pred": [[0.2, 0.13, 0.08]] } ' \
            '},' \
            '"uid2": { ' \
            '"trace_id1":' \
-           '{ "loc_true": [3], "loc_pred": [[4, 5]] }' \
+           '{ "loc_true": [0], "loc_pred": [[0.4, 0.5, 0.7]] }' \
            '}' \
            '}'
     lpt = lpem.LocationPredEvaluate(data, "DeepMove", "ACC", 1, 2)
