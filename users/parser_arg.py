2497c38cb922a513bb531b2007b6506f1ca501a8
shh2000 <13820618441@163.com>
2020-08-27 16:12:44 +0800

    fix dir

--- users/parser_arg.py
+++ users/parser_arg.py
@@ -7,7 +7,7 @@ import os
 def run_model_on_dataset(model, dataset):
     print(model, dataset)
     if model == 'deepmove' and dataset == 'foursquare':
-        os.system(r'cd D:\study\Bigscity-HMP-Toolkit\tasks && python evaluate_deep_move.py')
+        os.system(r'cd D:\study\Bigscity-HMP-Toolkit\tasks && del ..\model\save_model\checkpoint\ && python train_deep_move.py')
     else:
         print('You want to run {} on {}, no this model or datasets!'.format(model, dataset))
 
@@ -15,7 +15,7 @@ def run_model_on_dataset(model, dataset):
 def evaluate_model_on_dataset(model, dataset):
     print(model, dataset)
     if model == 'deepmove' and dataset == 'foursquare':
-        os.system(r'cd D:\study\Bigscity-HMP-Toolkit\tasks && python run_loc_pred_evaluate.py')
+        os.system(r'cd D:\study\Bigscity-HMP-Toolkit\tasks && python evaluate_deep_move.py')
     else:
         print('You want to evaluate {} on {}, no this model or datasets!'.format(model, dataset))
 
@@ -111,12 +111,13 @@ if __name__ == '__main__':
         for model in models2run['predict']:
             for dataset in datas2run:
                 run_model_on_dataset(model, dataset)
-                # evaluate_model_on_dataset(model, dataset)
+                evaluate_model_on_dataset(model, dataset)
 
         for model in models2run['plan']:
             for dataset in datas2run:
                 run_model_on_dataset(model, dataset)
-                # evaluate_model_on_dataset(model, dataset)
+                evaluate_model_on_dataset(model, dataset)
 
         print('Calculate finished! Begin generating report:')
+        
         exit(0)
