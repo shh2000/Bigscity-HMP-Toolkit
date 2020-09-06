8298aa5cba714f6c590000af02936ca56849ca90
Melles <842595644@qq.com>
2020-09-06 11:59:51 +0800

    finish transfer

--- README.md
+++ README.md
@@ -13,3 +13,8 @@ a toolkit for Location Prediction and Route Planning based on torch
 * gowalla数据集
 * foursquare数据集（checkins.dat）
 * foursquare数据集（[kaggle](https://www.kaggle.com/chetanism/foursquare-nyc-and-tokyo-checkin-dataset)）（目前使用该数据源作为 foursquare）
+* [FPMC 源码](https://github.com/khesui/FPMC)
+
+#### paper list
+* DeepMove: Predicting Human Mobility with Attentional Recurrent Networks
+* Factorizing Personalized Markov Chains for Next-Basket Recommendation
--- data_transfer/deep_move_transfer.py
+++ data_transfer/deep_move_transfer.py
@@ -4,33 +4,9 @@ import math
 import numpy as np
 from datetime import datetime, timedelta
 from sklearn.preprocessing import LabelEncoder
-
-def encodeLoc(loc):
-    return geohash2.encode(loc[0], loc[1]) # loc[0]: latitude, loc[1]: longtitude 
-
-def parseTime(time, time_format):
-    '''
-    parse to datetime
-    '''
-    # only implement 111111
-    if time_format[0] == '111111':
-        return datetime.strptime(time[0], '%Y-%m-%d-%H-%M-%S') # TODO: check if this is UTC Time ?
-
-def calculateBaseTime(start_time, base_zero):
-    if base_zero:
-        return start_time - timedelta(hours=start_time.hour, minutes=start_time.minute, seconds=start_time.second,microseconds=start_time.microsecond)
-    else:
-        # time length = 12
-        if start_time.hour < 12:
-            return start_time - timedelta(hours=start_time.hour, minutes=start_time.minute, seconds=start_time.second,microseconds=start_time.microsecond)
-        else:
-            return start_time - timedelta(hours=start_time.hour - 12, minutes=start_time.minute, seconds=start_time.second,microseconds=start_time.microsecond)
-
-def calculateTimeOff(now_time, base_time):
-    # 先将 now 按小时对齐
-    now_time = now_time - timedelta(minutes=now_time.minute, seconds=now_time.second)
-    delta = now_time - base_time
-    return delta.days * 24 + delta.seconds / 3600
+import sys
+sys.path.append('..')
+from utils import encodeLoc, parseTime, calculateBaseTime, calculateTimeOff
 
 def deepMoveTransfer(data, min_session_len = 5, min_sessions = 2, time_length = 72):
     '''
