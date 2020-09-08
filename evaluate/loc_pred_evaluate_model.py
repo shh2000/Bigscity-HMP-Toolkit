81623499d8fc9d31408a29eb58acaadc210b2fc3
飞翔的小兔子 <1290830173@qq.com>
2020-09-08 13:06:50 +0800

    保存评估结果

--- evaluate/loc_pred_evaluate_model.py
+++ evaluate/loc_pred_evaluate_model.py
@@ -7,12 +7,19 @@ from evaluate import loc_pred_evaluate_data as lped
 
 class LocationPredEvaluate(object):
     def __init__(self, data, datatype='DeepMove', mode='ACC', k=1, len_pred=1):
+        # 传入的数据参数以及评估参数
         self.data = data
         self.datatype = datatype
         self.mode = mode
         self.k = k
         self.len_pred = len_pred
         self.init_data()
+        # 保存的评估结果
+        self.evaluated = False
+        self.acc_list = []
+        self.loss_list = []
+        self.avg_acc = 0
+        self.avg_loss = 0
 
     def init_data(self):
         """
@@ -61,51 +68,48 @@ class LocationPredEvaluate(object):
                 break
         return ids_list
 
-    '''
-    预测评价指标，参考材料 https://blog.csdn.net/guolindonggld/article/details/87856780
-    MSE：均方误差（Mean Square Error）
-    MAE：平均绝对误差（Mean Absolute Error）
-    RMSE：均方根误差（Root Mean Square Error）
-    MAPE：平均绝对百分比误差（Mean Absolute Percentage Error）
-    MARE：平均绝对和相对误差（Mean Absolute Relative Error）
-    SMAPE：对称平均绝对百分比误差（Symmetric Mean Absolute Percentage Error）
-    '''
-
     # 均方误差（Mean Square Error）
-    def MSE(self, loc_pred, loc_true):
+    @staticmethod
+    def MSE(loc_pred, loc_true):
         assert len(loc_pred) == len(loc_true), 'MSE: 预测数据与真实数据大小不一致'
         return np.mean(sum(pow(loc_pred - loc_true, 2)))
 
     # 平均绝对误差（Mean Absolute Error）
-    def MAE(self, loc_pred, loc_true):
+    @staticmethod
+    def MAE(loc_pred, loc_true):
         assert len(loc_pred) == len(loc_true), 'MAE: 预测数据与真实数据大小不一致'
         return np.mean(sum(loc_pred - loc_true))
 
     # 均方根误差（Root Mean Square Error）
-    def RMSE(self, loc_pred, loc_true):
+    @staticmethod
+    def RMSE(loc_pred, loc_true):
         assert len(loc_pred) == len(loc_true), 'RMSE: 预测数据与真实数据大小不一致'
         return np.sqrt(np.mean(sum(pow(loc_pred - loc_true, 2))))
 
     # 平均绝对百分比误差（Mean Absolute Percentage Error）
-    def MAPE(self, loc_pred, loc_true):
+    @staticmethod
+    def MAPE(loc_pred, loc_true):
         assert len(loc_pred) == len(loc_true), 'MAPE: 预测数据与真实数据大小不一致'
         assert 0 not in loc_true, "MAPE: 真实数据有0，该公式不适用"
         return np.mean(abs(loc_pred - loc_true) / loc_true)
 
     # 平均绝对和相对误差（Mean Absolute Relative Error）
-    def MARE(self, loc_pred, loc_true):
+    @staticmethod
+    def MARE(loc_pred, loc_true):
         assert len(loc_pred) == len(loc_true), "MARE：预测数据与真实数据大小不一致"
         assert np.sum(loc_true) != 0, "MARE：真实位置全为0，该公式不适用"
         return np.sum(np.abs(loc_pred - loc_true)) / np.sum(loc_true)
 
     # 对称平均绝对百分比误差（Symmetric Mean Absolute Percentage Error）
-    def SMAPE(self, loc_pred, loc_true):
+    @staticmethod
+    def SMAPE(loc_pred, loc_true):
         assert len(loc_pred) == len(loc_true), 'SMAPE: 预测数据与真实数据大小不一致'
         assert 0 in (loc_pred + loc_true), "SMAPE: 预测数据与真实数据有0，该公式不适用"
         return 2.0 * np.mean(np.abs(loc_pred - loc_true) / (np.abs(loc_pred) + np.abs(loc_true)))
 
     # 对比真实位置与预测位置获得预测准确率
-    def get_acc(self, loc_pred, loc_true):
+    @staticmethod
+    def ACC(loc_pred, loc_true):
         assert len(loc_pred) == len(loc_true), "accuracy: 预测数据与真实数据大小不一致"
         loc_diff = loc_pred - loc_true
         loc_diff[loc_diff != 0] = 1
@@ -116,12 +120,12 @@ class LocationPredEvaluate(object):
         assert len(loc_pred) >= self.k, "top-k ACC评估方法：没有提供足够的预测数据做评估"
         assert len(loc_pred[0]) == len(loc_true), "top-k ACC评估方法：预测数据与真实数据大小不一致"
         if self.k == 1:
-            t, avg_acc = self.get_acc(loc_pred[0], loc_true)
+            t, avg_acc = self.ACC(loc_pred[0], loc_true)
             return avg_acc
         else:
             tot_list = np.zeros(len(loc_true), dtype=int)
             for i in range(self.k):
-                t, avg_acc = self.get_acc(loc_pred[i], loc_true)
+                t, avg_acc = self.ACC(loc_pred[i], loc_true)
                 tot_list = tot_list + t
             return np.mean(tot_list < self.k)
 
@@ -135,40 +139,57 @@ class LocationPredEvaluate(object):
             avg_acc = self.topk(np.array(loc_pred2, dtype=object), np.array(loc_true, dtype=object))
             if field == 'model':
                 print('---- 该模型在 top-{} ACC 评估方法下 avg_acc={:.3f} ----'.format(self.k, avg_acc))
+                self.avg_acc = avg_acc
             else:
                 print('accuracy={:.3f}'.format(avg_acc))
+                self.acc_list.append(avg_acc)
         elif self.mode == 'RMSE':
             avg_loss = self.RMSE(np.array(loc_pred2[0]), np.array(loc_true))
             if field == 'model':
                 print('---- 该模型在 RMSE 评估方法下 avg_loss={:.3f} ----'.format(avg_loss))
+                self.avg_loss = avg_loss
             else:
                 print('RMSE avg_loss={:.3f}'.format(avg_loss))
+                self.loss_list.append(avg_loss)
         elif self.mode == "MSE":
             avg_loss = self.MSE(np.array(loc_pred2[0]), np.array(loc_true))
             if field == 'model':
                 print('---- 该模型在 MSE 评估方法下 avg_loss={:.3f} ----'.format(avg_loss))
+                self.avg_loss = avg_loss
             else:
                 print('MSE avg_loss={:.3f}'.format(avg_loss))
+                self.loss_list.append(avg_loss)
         elif self.mode == "MAE":
             avg_loss = self.MAE(np.array(loc_pred2[0]), np.array(loc_true))
             if field == 'model':
                 print('---- 该模型在 MAE 评估方法下 avg_loss={:.3f} ----'.format(avg_loss))
+                self.avg_loss = avg_loss
             else:
                 print('MAE avg_loss={:.3f}'.format(avg_loss))
+                self.loss_list.append(avg_loss)
         elif self.mode == "MAPE":
             avg_loss = self.MAPE(np.array(loc_pred2[0]), np.array(loc_true))
             if field == 'model':
                 print('---- 该模型在 MAPE 评估方法下 avg_loss={:.3f} ----'.format(avg_loss))
+                self.avg_loss = avg_loss
             else:
                 print('MAPE avg_loss={:.3f}'.format(avg_loss))
+                self.loss_list.append(avg_loss)
         elif self.mode == "MARE":
             avg_loss = self.MARE(np.array(loc_pred2[0]), np.array(loc_true))
             if field == 'model':
                 print('---- 该模型在 MARE 评估方法下 avg_loss={:.3f} ----'.format(avg_loss))
+                self.avg_loss = avg_loss
             else:
                 print('MARE avg_loss={:.3f}'.format(avg_loss))
+                self.loss_list.append(avg_loss)
 
     def run(self):
+        """
+        评估入口，根据评估类的参数做相应的评估
+        :return:
+        """
+        self.evaluated = True
         test_data_set = lped.LPEDataset(self.data)
         test_data_loader = DataLoader(dataset=test_data_set, batch_size=1, shuffle=True)
         # 用户轨迹列表
@@ -192,3 +213,23 @@ class LocationPredEvaluate(object):
                 print('trace_id {} : '.format(trace_id), end="")
                 self.run_evaluate(t_loc_pred, t_loc_true, 'trace')
         self.run_evaluate(loc_pred, loc_true, 'model')
+
+    def get_acc(self):
+        assert self.evaluated, "还未对数据进行评估，请运行模型的run方法"
+        assert self.mode == "ACC", self.mode + "评估方法对应的指标为avg_loss"
+        return self.avg_acc
+
+    def get_loss(self):
+        assert self.evaluated, "还未对数据进行评估，请运行模型的run方法"
+        assert self.mode != "ACC", self.mode + "评估方法对应的指标为avg_acc"
+        return self.avg_loss
+
+    def get_acc_list(self):
+        assert self.evaluated, "还未对数据进行评估，请运行模型的run方法"
+        assert self.mode == "ACC", self.mode + "评估方法对应的指标为avg_loss"
+        return self.acc_list
+
+    def get_loss_list(self):
+        assert self.evaluated, "还未对数据进行评估，请运行模型的run方法"
+        assert self.mode != "ACC", self.mode + "评估方法对应的指标为avg_acc"
+        return self.loss_list
